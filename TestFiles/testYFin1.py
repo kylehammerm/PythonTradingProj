b802fabd9b50823968982cfaf714d0b2a439d8f7
kylehammerm <kylehammerm@gmail.com>
2023-01-17 23:43:48 -0500

    Organized files

--- TestFiles/testYFin1.py
+++ TestFiles/testYFin1.py
@@ -8,9 +8,9 @@ interval = '1d'
 # Ticker we are using
 stock = "TSLA"
 
+
 # says whether to buy or sell
 def signal_generator(dataframe):
-
     # Gets the open and close of the last day
     open = dataframe.Open.iloc[-1]
     close = dataframe.Close[-1]
@@ -20,14 +20,16 @@ def signal_generator(dataframe):
     prevClose = dataframe.Close[-2]
 
     # looks for a bearish pattern
-    if open>close and prevOpen<prevClose and close<prevOpen and open>= prevClose:
+    if open > close and prevOpen < prevClose and close < prevOpen and open >= prevClose:
         return 1
     # looks for bullish pattern
-    elif open<close and prevOpen>prevClose and close>prevOpen and open<= prevClose:
+    elif open < close and prevOpen > prevClose and close > prevOpen and open <= prevClose:
         return 2
     # no clear pattern
     else:
         return 0
+
+
 # gets the data for our interval
 dataF = yf.download(stock, start=startDate, end=curDate, interval=interval)
 print(type(dataF))
@@ -35,9 +37,9 @@ print(type(dataF))
 sig = []
 sig.append(0)
 
-for i in range(1,len(dataF)):
+for i in range(1, len(dataF)):
     # gets last two days
-    df = dataF[i-1:i+1]
+    df = dataF[i - 1:i + 1]
     # generates signal for the last two days
     sig.append(signal_generator(df))
 
---
+++ TestFiles/testyfin2.py
@@ -0,0 +1,49 @@
+
+import pandas as pd
+
+import yfinance as yf
+
+
+def signal_generator(df):
+    # Gets the open and close for today
+    open = df.Open.iloc[-1]
+    close = df.Close.iloc[-1]
+
+    # Gets the open and close from yesterday
+    previous_open = df.Open.iloc[-2]
+    previous_close = df.Close.iloc[-2]
+
+    # Bearish Pattern
+    if open > close and close < previous_open < previous_close <= open:
+        return 1
+
+    # Bullish Pattern
+    elif open < close and close > previous_open > previous_close >= open:
+        return 2
+
+    # No clear Pattern
+    else:
+        return 0
+
+
+curr_date = '2023-01-17'
+start_date = '2023-01-01'
+interval = '15m'
+
+stock = 'TSLA'
+
+data_df = yf.download(stock, start=start_date, end=curr_date, interval=interval)
+print(type(data_df))
+
+signal = [0]
+for i in range(1, len(data_df)):
+    # Gets last two days
+    df = data_df[i-1:i+1]
+
+    # Generates the signal for the two days
+    signal.append(signal_generator(df))
+
+# Adds the signals to the data frame
+data_df['signal'] = signal
+
+print(data_df.signal.value_counts())
