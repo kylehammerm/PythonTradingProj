2a1e43e0496b261cafc7ffd2582ea849db8ebe3c
Kasen Groth <ktgroth03@gmail.com>
2023-01-19 21:56:10 -0500

    Moved the mock bot into its own class, and tested the returns for NVDA over one month

--- TestFiles/testyfin2.py
+++ TestFiles/testyfin2.py
@@ -30,9 +30,9 @@ curr_date = '2023-01-01'
 start_date = '2022-12-01'
 interval = '2m'
 
-stock = 'TSLA'
+stock_symbol = 'NVDA'
 
-data_df = yf.download(stock, start=start_date, end=curr_date, interval=interval)
+data_df = yf.download(stock_symbol, start=start_date, end=curr_date, interval=interval)
 print(type(data_df))
 
 signal = [0]
@@ -48,24 +48,37 @@ data_df['Signal'] = signal
 
 print(data_df.Signal.value_counts())
 
-stock_total = 0
-stock = 0
-wallet = 1000
-for i in range(len(data_df['Signal'])):
-    match data_df['Signal'][i]:
-        case 0:
-            print(f"[UNCLEAR] Holding: {data_df['Close'][i]}")
-        case 1:
-            print(f"[BEARISH] Selling: {data_df['Close'][i]}")
-            wallet += stock * data_df['Close'][i]
-            stock = 0
-        case 2:
-            print(f"[BULLISH] Buying: {data_df['Close'][i]}")
-            newStock = wallet // data_df['Close'][i]
-            wallet -= data_df['Close'][i] * newStock
-            stock += newStock
-            stock_total += stock
+def mock_bot(wallet: int, time: str, verbose: bool):
+    stock = 0
+    history = []
+    for i in range(len(data_df['Signal'])):
+        f = i + (0 if time == 'Close' else 1)
+        try:
+            match data_df['Signal'][f]:
+                case 0:
+                    print(f"[UNCLEAR] Holding: {data_df[time][f]}")
+                case 1:
+                    print(f"[BEARISH] Selling: {data_df[time][f]}")
+                    wallet += stock * data_df[time][f]
+                    stock = 0
+                case 2:
+                    print(f"[BULLISH] Buying: {data_df[time][f]}")
+                    newStock = wallet // data_df[time][f]
+                    wallet -= data_df[time][f] * newStock
+                    stock += newStock
+        except IndexError:
+            pass
+        history.append({'wallet': wallet, 'stock': stock});
+
+    wallet += stock * data_df[time][len(data_df[time])-1]
+    if verbose:
+        print(history)
+    return wallet
 
 
-wallet += stock * data_df['Close'][len(data_df['Close'])-1]
-print(wallet, stock_total)
+initial = 1000
+w1 = mock_bot(initial, 'Open', 0)
+w2 = mock_bot(initial, 'Close', 0)
+
+print(w1, w2, w1-w2)
+print((w1-initial)/initial*100, (w2-initial)/initial*100, (w1-w2)/initial*100)
