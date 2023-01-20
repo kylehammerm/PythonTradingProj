3b5e21691c5f872df7eb2cd5e6527580d159335d
kylehammerm <kylehammerm@gmail.com>
2023-01-19 23:32:48 -0500

    Updates the testStrat1

--- TestFiles/testStrat1.py
+++ TestFiles/testStrat1.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from matplotlib import pyplot as plt
 import yfinance as yf
 
@@ -24,11 +25,11 @@ def signal_generator(df):
         return 0
 
 
-curr_date = '2023-01-01'
-start_date = '2022-12-01'
+curr_date = '2023-01-19'
+start_date = '2022-12-10'
 interval = '2m'
 
-stock_symbol = 'NVDA'
+stock_symbol = 'PLTR'
 
 data_df = yf.download(stock_symbol, start=start_date, end=curr_date, interval=interval)
 print(type(data_df))
@@ -48,46 +49,52 @@ print(data_df.Signal.value_counts())
 
 def mock_bot(wallet: int, time: str, verbose: bool):
     stock = 0
+    total_stock = 0
     history = []
     for i in range(len(data_df['Signal'])):
         f = i + (0 if time == 'Close' else 1)
         try:
             match data_df['Signal'][f]:
                 case 0:
-                    print(f"[UNCLEAR] Holding: {data_df[time][f]}")
+                    if verbose:
+                        print(f"[UNCLEAR] Holding: {data_df[time][f]}")
                 case 1:
-                    print(f"[BEARISH] Selling: {data_df[time][f]}")
+                    if verbose:
+                        print(f"[BEARISH] Selling: {data_df[time][f]}")
                     wallet += stock * data_df[time][f]
                     stock = 0
                 case 2:
-                    print(f"[BULLISH] Buying: {data_df[time][f]}")
-                    newStock = wallet // data_df[time][f]
+                    if verbose:
+                        print(f"[BULLISH] Buying: {data_df[time][f]}")
+                    newStock = wallet / data_df[time][f]
                     wallet -= data_df[time][f] * newStock
                     stock += newStock
+                    total_stock += newStock
         except IndexError:
             pass
-        history.append({'wallet': wallet, 'stock': stock});
+        history.append({'wallet': wallet, 'stock': stock})
 
     wallet += stock * data_df[time][len(data_df[time])-1]
     if verbose:
         print(history)
-    return wallet
+    return wallet, total_stock, history
 
 
 initial = 1000
-w1 = mock_bot(initial, 'Open', 0)
-w2 = mock_bot(initial, 'Close', 0)
+w1, t1, h1 = mock_bot(initial, 'Open', False)
+w2, t2, h2 = mock_bot(initial, 'Close', False)
 
-print(w1, w2, w1-w2)
-print((w1-initial)/initial*100, (w2-initial)/initial*100, (w1-w2)/initial*100)
+print(f"Open Strategy: ${w1}", f"Close Strategy: ${w2}", f"Difference: ${w1-w2}")
+print(f"Open Strategy Gain: {(w1-initial)/initial*100}%", f"Close Strategy Gain: {(w2-initial)/initial*100}%", f"Difference: {(w1-w2)/initial*100}%")
+print(f"Total Stock Open Strategy: {t1}", f"Total Stock Close Strategy: {t2}", f"Difference: {t1-t2}")
 color = "g"
 for i in range(1, len(signal)):
 
     # decides the color based on whether we are holding the stock or not
-    if signal[i] == 1:
-        color = "r"
-    elif signal[i] == 2:
+    if h1[i]["stock"] > 0:
         color = "g"
+    elif h1[i]["stock"] == 0:
+        color = "r"
     else:
         color = color
     # red means don't have the stock green means we own it
